c21e8077ffa9f3a4378e5e39c48971a9d5013623
Tony Dinh <tony@tdinh.me>
2021-12-09 21:51:54 +0700

    Didn't work. Revert

--- pages/subscribe.js
+++ pages/subscribe.js
@@ -75,24 +75,6 @@ export default function Subscribe() {
           }
         `}
       </style>
-      <style global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        @media (prefers-color-scheme: dark) {
-          html,
-          body {
-            color-scheme: dark;
-            color: white;
-          }
-        }
-      `}</style>
     </div>
   );
 }
