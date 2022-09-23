c0da69911c02affeb315d86db1e5c0f1c0a4379b
Tony Dinh <tony-git@tdinh.me>
2022-09-23 07:13:38 +0700

    Contact me

---
+++ pages/contact.js
@@ -0,0 +1,8 @@
+export default function Redirector() {
+  if (typeof window === 'object') {
+    window.location.href =
+      'https://tdinh.notion.site/Here-are-things-people-ask-me-the-most-in-my-DM-9bae59963bde43caacba44edf295d980';
+  }
+
+  return null;
+}
